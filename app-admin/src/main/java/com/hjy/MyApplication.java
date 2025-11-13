186f501733687e6c6670086cb96d462e44d8496c
hjy <584153255@qq.com>
2025-11-13 09:55:47 +0800

    no message

--- app-admin/src/main/java/com/hjy/MyApplication.java
+++ app-admin/src/main/java/com/hjy/MyApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"com.hjy"})
-//@MapperScan()
 @MapperScan({"com.hjy.**.mapper"})
 public class MyApplication {
     public static void main(String[] args) {
--- app-admin/src/main/java/com/hjy/web/controller/system/SysConfigController.java
+++ app-admin/src/main/java/com/hjy/web/controller/system/SysConfigController.java
@@ -1,16 +1,18 @@
 package com.hjy.web.controller.system;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hjy.system.domain.SysConfig;
 import com.hjy.system.service.ISysConfigService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/system")
+@RequestMapping("/system/config")
 public class SysConfigController {
 
     @Autowired
@@ -20,6 +22,11 @@ public class SysConfigController {
     public List<SysConfig> getSysConfigList(){
 //        sysConfigService.list();
         return sysConfigService.list();
+    }
+
+    @GetMapping( "/listPage")
+    public Page<SysConfig> listPage(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10") Integer size){
+        return sysConfigService.page(new Page<>(page,size));
     }
 
 
