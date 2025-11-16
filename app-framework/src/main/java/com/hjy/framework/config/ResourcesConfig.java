6290c6ab6d65dc76f7b027d70444f8a96a5b0c5c
HJY-PC\Administrator <584153255@qq.com>
2025-11-16 18:05:12 +0800

    no message

--- app-framework/src/main/java/com/hjy/framework/config/ResourcesConfig.java
+++ app-framework/src/main/java/com/hjy/framework/config/ResourcesConfig.java
@@ -11,16 +11,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class ResourcesConfig implements WebMvcConfigurer {
 
     @Bean
-    public CorsFilter corsFilter()
-    {
+    public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         // 设置访问源地址
+        // "http://localhost:8081"
         config.addAllowedOriginPattern("*");
         // 设置访问源请求头
+        // example:"token,content-text"
         config.addAllowedHeader("*");
         // 设置访问源请求方法
+        // example:post,put
         config.addAllowedMethod("*");
         // 有效期 1800秒
+//        // OPTIONS
+//        浏览器会先发一个"预检请求"(OPTIONS请求)去问服务器："嘿，我能不能跨域访问你的资源呀？"
+//
+//        这个setMaxAge(1800L)就是告诉浏览器："放心吧，我允许你问1800秒（30分钟）！在这30分钟内，
+//        你不用再问我，直接用之前问过的结果就行。"
         config.setMaxAge(1800L);
         // 添加映射路径，拦截一切请求
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
